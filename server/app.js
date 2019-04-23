73e28401d431a2c35f52413860b0d7ce90d0265e
bahu1013 <xu.yong@yandex.com>
2019-04-23 21:29:19 +0800

    Initial commit

--- server/app.js
+++ server/app.js
@@ -40,4 +40,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 module.exports = app;
